0878b5c4e1ab6229787cd8c2555f2f11f71b836a
Melnyk Nikolay <nikmlk87@gmail.com>
2018-10-14 14:05:43 +0300

    start project

---
+++ src/main/java/com/itstep/caloriesmanager/Main.java
@@ -0,0 +1,7 @@
+package com.itstep.caloriesmanager;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello Topjava Enterprise!");
+    }
+}
---
+++ src/main/java/com/itstep/caloriesmanager/util/TimeUtil.java
@@ -0,0 +1,9 @@
+package com.itstep.caloriesmanager.util;
+
+import java.time.LocalTime;
+
+public class TimeUtil {
+    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
+    }
+}
