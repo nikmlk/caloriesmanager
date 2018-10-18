82a481f157f6c1b11e8a9ceb73ee64361e52439b
Melnyk Nikolay <nikmlk87@gmail.com>
2018-10-18 20:32:41 +0300

    4-forward_to_redirect

--- src/main/java/com/itstep/caloriesmanager/web/UserServlet.java
+++ src/main/java/com/itstep/caloriesmanager/web/UserServlet.java
@@ -10,6 +10,6 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
-    }
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("users.jsp");    }
 }
