9115f2852e47ceb9329b3e7592e0c6433d5d6a04
nikmlk <nikmlk@mail.ru>
2018-10-27 00:36:37 -0700

    1

--- src/main/java/com/itstep/caloriesmanager/util/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package com.itstep.caloriesmanager.util;
-
-import java.time.LocalTime;
-
-public class TimeUtil {
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
-    }
-}
