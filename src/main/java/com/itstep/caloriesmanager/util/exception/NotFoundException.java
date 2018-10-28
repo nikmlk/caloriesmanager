3a9154cf6d9921feb69cc50d216cc74fd3e28e03
nikmlk <nikmlk@mail.ru>
2018-10-28 06:48:19 -0700

    3

---
+++ src/main/java/com/itstep/caloriesmanager/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.itstep.caloriesmanager.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
