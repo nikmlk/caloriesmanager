f38f7e4c770676f98986ced62c720773df1cc5b7
nikmlk <nikmlk@mail.ru>
2018-10-28 05:00:20 -0700

    5

--- src/main/java/com/itstep/caloriesmanager/SpringMain.java
+++ src/main/java/com/itstep/caloriesmanager/SpringMain.java
@@ -1,6 +1,9 @@
 package com.itstep.caloriesmanager;
 
+import com.itstep.caloriesmanager.model.Role;
+import com.itstep.caloriesmanager.model.User;
 import com.itstep.caloriesmanager.repository.UserRepository;
+import com.itstep.caloriesmanager.service.UserService;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,6 +17,10 @@ public class SpringMain {
 //        UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
         UserRepository userRepository = appCtx.getBean(UserRepository.class);
         userRepository.getAll();
+
+        UserService userService = appCtx.getBean(UserService.class);
+        userService.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+
         appCtx.close();
     }
 }
