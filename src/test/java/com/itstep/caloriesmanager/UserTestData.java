2b5f662e82c9dd2a0cecd3d9a446a6960fad7d44
nikmlk <nikmlk@mail.ru>
2018-11-04 01:06:04 -0700

    6

--- src/test/java/com/itstep/caloriesmanager/UserTestData.java
+++ src/test/java/com/itstep/caloriesmanager/UserTestData.java
@@ -3,10 +3,28 @@ package com.itstep.caloriesmanager;
 import com.itstep.caloriesmanager.model.Role;
 import com.itstep.caloriesmanager.model.User;
 
+import java.util.Arrays;
+
+import static com.itstep.caloriesmanager.model.AbstractBaseEntity.START_SEQ;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserTestData {
-    public static final int USER_ID = 1;
-    public static final int ADMIN_ID = 2;
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
 
     public static final User USER = new User(USER_ID, "User", "user@yandex.ru", "password", Role.ROLE_USER);
     public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN);
+
+    public static void assertMatch(User actual, User expected) {
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "registered", "roles");
+    }
+
+    public static void assertMatch(Iterable<User> actual, User... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<User> actual, Iterable<User> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("registered", "roles").isEqualTo(expected);
+    }
 }
+
