9367d63e3606474f2186e9090443a50e887f531b
nikmlk <nikmlk@mail.ru>
2018-11-04 04:36:23 -0800

    3

---
+++ src/test/java/com/itstep/caloriesmanager/web/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,47 @@
+package com.itstep.caloriesmanager.web;
+
+import com.itstep.caloriesmanager.UserTestData;
+import com.itstep.caloriesmanager.model.User;
+import com.itstep.caloriesmanager.repository.inmemory.InMemoryUserRepositoryImpl;
+import com.itstep.caloriesmanager.util.exception.NotFoundException;
+import com.itstep.caloriesmanager.web.user.AdminRestController;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+
+import static com.itstep.caloriesmanager.UserTestData.ADMIN;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+
+    @Autowired
+    private AdminRestController controller;
+
+    @Autowired
+    private InMemoryUserRepositoryImpl repository;
+
+    @Before
+    public void setUp() throws Exception {
+        repository.init();
+    }
+
+    @Test
+    public void delete() throws Exception {
+        controller.delete(UserTestData.USER_ID);
+        Collection<User> users = controller.getAll();
+        Assert.assertEquals(users.size(), 1);
+        Assert.assertEquals(users.iterator().next(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deleteNotFound() throws Exception {
+        controller.delete(10);
+    }
+}
