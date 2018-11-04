c6a115e9b1861feadd4eb3b9ffd5dfd76dfc0f8d
nikmlk <nikmlk@mail.ru>
2018-11-04 01:38:48 -0700

    HW-optional

--- src/test/java/com/itstep/caloriesmanager/repository/inmemory/InMemoryMealRepositoryImpl.java
+++ src/test/java/com/itstep/caloriesmanager/repository/inmemory/InMemoryMealRepositoryImpl.java
@@ -33,14 +33,6 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
-    {
-        MealsUtil.MEALS.forEach(meal -> save(meal, USER_ID));
-
-        save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510), ADMIN_ID);
-        save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500), ADMIN_ID);
-    }
-
-
     @Override
     public Meal save(Meal meal, int userId) {
         Map<Integer, Meal> meals = repository.computeIfAbsent(userId, ConcurrentHashMap::new);
