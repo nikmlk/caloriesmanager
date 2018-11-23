601dc0936ebb75545b5a56c22288575b945a7a8f
nikmlk <nikmlk@mail.ru>
2018-11-23 07:12:34 -0800

    15

---
+++ src/main/java/com/itstep/caloriesmanager/web/RootController.java
@@ -0,0 +1,34 @@
+package com.itstep.caloriesmanager.web;
+
+import com.itstep.caloriesmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RootController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        return "redirect:meals";
+    }
+}
