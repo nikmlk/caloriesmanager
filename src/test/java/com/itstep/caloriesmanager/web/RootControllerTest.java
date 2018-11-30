aa1c7df604eda68677a01e1c927a404931c1f5fc
nikmlk <nikmlk@mail.ru>
2018-11-30 10:34:11 -0800

    7

---
+++ src/test/java/com/itstep/caloriesmanager/web/RootControllerTest.java
@@ -0,0 +1,31 @@
+package com.itstep.caloriesmanager.web;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static com.itstep.caloriesmanager.UserTestData.USER;
+import static com.itstep.caloriesmanager.model.AbstractBaseEntity.START_SEQ;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class RootControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testUsers() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
